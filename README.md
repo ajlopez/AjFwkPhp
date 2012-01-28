41be34b0f1c68466ec026edd4594f9ebcf19dff0
ajlopez <webmaster@ajlopez.com>
2012-01-28 16:13:28 -0300

    Code taken from AjGenesis, examples/EntityExamples/SourceCode

---
+++ README.md
@@ -0,0 +1,10 @@
+# AjFwkPhp
+
+Improving my old but simple PHP Framework. 
+
+Based on version 0.2, used in 
+[AjGenesis](http://ajgenesis.codeplex.com/)
+, EntityExamples/SourceCode.
+
+Work in Progress
+
---
+++ src/includes/Bottom.inc.php
@@ -0,0 +1,17 @@
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+<center>
+<table border="0" cellpadding="0" cellspacing="0" class="bottom" width="100%">
+<tr><TD align="center" valign="bottom" width="100%">
+Programado por Angel J. Lopez <a href='http://www.ajlopez.com'>www.ajlopez.com</a>
+ </TD></tr>
+</table>
+</center>
+</BODY>
+</HTML>
+
+
